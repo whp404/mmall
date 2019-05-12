102d45e8b8227529c2688f86e823fd72dd25cbad
whp404 <whp404@sina.com>
2019-05-12 23:12:56 +0800

    'redis和json工具'

---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,66 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = ".happymall.com";
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie ck : cookies) {
+                log.info("read cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                    return ck.getValue();
+                }
+            }
+        }
+        return null;
+
+    }
+
+    /**
+     * 将token 往cookie中写入
+     *
+     * @param response
+     * @param token
+     */
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        cookie.setPath("/");//代表只有根目录下的代码才能访问cookie
+        cookie.setHttpOnly(true);
+
+        cookie.setMaxAge(60 * 60 * 24 * 365);//-1代表永不过期
+        log.info("write cookieName:{},cookieValue:{}", cookie.getName(), cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (StringUtils.equals(c.getName(), COOKIE_NAME)) {
+                    c.setDomain(COOKIE_DOMAIN);
+                    c.setPath("/");
+                    c.setMaxAge(0);
+                    log.info("del cookieName:{},cookieValue:{}", c.getName(), c.getValue());
+                    response.addCookie(c);
+                    return;
+                }
+            }
+        }
+
+    }
+
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
+<h2>TOMCAT 1</h2>
 <h2>Hello World!</h2>
 </body>
 </html>
